b9e3dfbcbbe7b9692c175fa3009412c4b12e3689
Aqeel8211 <m.aqeelch8211@gmail.com>
2026-04-12 19:24:16 +0500

    Initial commit: Nexus Platform Backend API

---
+++ DTOs/AuthResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Nexus_backend.DTOs
+{
+    public class AuthResponseDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public UserDto? User { get; set; }
+    }
+}
